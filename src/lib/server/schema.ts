b9cb8cd0f4f4ddcc73c67b9225d1f3adf8ef941e
Saahil <neon@saahild.com>
2025-06-20 21:56:51 -0400

    prisma init or smt

--- src/lib/server/schema.ts
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core"
-
-export const users = sqliteTable('users', {
-  id: integer(),
-  githubId: integer('github_id').notNull().primaryKey(),
-    githubLogin: text('github_login').notNull(),
-    githubAvatarUrl: text('github_avatar_url').notNull(),
-  
-});
