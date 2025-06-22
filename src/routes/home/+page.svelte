74df7df2b76179149959c08e436cd272a95e9f4f
Saahil <neon@saahild.com>
2025-06-22 17:10:16 -0400

    more work :3

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  //   import { page } from "$app/stores"
-  // import {  } from "@auth/sveltekit"
+  import CreateModal from "$lib/components/CreateModal.svelte";
+  import { Modal, Content, Trigger } from "sv-popup"
   export let data;
   // idk a page of repos maybe?
   export let projects = data.projects || [];
@@ -14,12 +14,21 @@
     <p>projects existo</p>
   {:else}
     <div>
+      <Modal>
+        <Content>
+          <CreateModal />
+        </Content>
+       <Trigger>
       <button
         class="btn btn-primary p-2 mt-10"
         on:click={() => {
           // fetch to form or smt
+          console.log("Create a project");
         }}>Create a project, You have none right now :3</button
       >
+    </Trigger>
+    </Modal>
     </div>
+
   {/if}
 </div>
